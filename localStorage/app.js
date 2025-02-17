c803fd032e8ca584e1c7bec8be16505cd9941802
Kreucyu <elciocleiton72@gmail.com>
2025-02-16 23:42:33 -0300

    projeto localStorage e JSON concluído

--- localStorage/app.js
+++ localStorage/app.js
@@ -37,6 +37,41 @@ class Registro {
         localStorage.setItem(id, JSON.stringify(tarefa))
         localStorage.setItem('id', id)
     }
+    //retorna todas as tarefas cadastradas
+    receberTarefas() {
+        let tarefas = []
+        let id = localStorage.getItem('id')
+        for(let i = 1; i <= id; i++) {
+            let tarefa = JSON.parse(localStorage.getItem(i))
+            if(tarefa === null) {
+                continue
+            }
+            tarefa.id = i
+            tarefas.push(tarefa)
+        }
+        return tarefas
+    }
+
+    procurar(tarefa) {
+        let filtroTarefas = this.receberTarefas()
+        filtroTarefas = filtroTarefas.filter(t => t.nome === nomeUsuario)
+        filtroTarefas
+        if (tarefa.descricao != '') {
+            filtroTarefas = filtroTarefas.filter(t => t.descricao == tarefa.descricao)
+        }if (tarefa.data != '') {
+            filtroTarefas = filtroTarefas.filter(t => t.data == tarefa.data)
+        }if (tarefa.prioridade != '') {
+            filtroTarefas = filtroTarefas.filter(t => t.prioridade == tarefa.prioridade)
+        }if (tarefa.status != '') {
+            filtroTarefas = filtroTarefas.filter(t => t.status == tarefa.status)
+        }
+        return filtroTarefas
+    }
+
+    remover (id) {
+        localStorage.removeItem(id)
+    }
+
 }
 
 //objeto que envia o registro de dados ao LocalStorage
@@ -51,25 +86,24 @@ function salvarUsuario() {
         //se o valor for válido, será atribuido ao nome
         nomeUsuario = testador.value.toLowerCase()
         sessionStorage.setItem("nome", nomeUsuario)
+        receberDados(2)
     } else {
         exibirModal(1, 'erro')
     }
 }
 
 function receberDados(valor) {
+
+    //guarda os dados da tarefa em variáveis pelo DOM
+    let descricao = document.getElementById('descricao')
+    let date = document.getElementById('data')
+    let prioridade = document.getElementById('prioridade')
+    let status = document.getElementById('status')
+    //instancia uma nova tarefa passando as variáveis com dados
+    let tarefa = new Tarefa(nomeUsuario, descricao.value, data.value, prioridade.value, status.value)
+
     switch (valor) {
         case 1:
-            //guarda os dados da tarefa em variáveis pelo DOM
-            let descricao = document.getElementById('descricao')
-            let data = document.getElementById('data')
-            let prioridade = document.getElementById('prioridade')
-            let status = document.getElementById('status')
-
-            //instancia uma nova tarefa passando as variáveis com dados
-            let tarefa = new Tarefa(nomeUsuario, descricao.value, data.value, prioridade.value, status.value)
-
-
-
             if (tarefa.validarDados()) {
                 //envia a chamada do método que salva os dados
                 registro.salvarDados(tarefa)
@@ -84,15 +118,44 @@ function receberDados(valor) {
             }
             break;
         case 2:
-            
-
-            break;
-        case 3:
-            
+            let tarefas = registro.procurar(tarefa)
+            exibirListaTarefas(tarefas, true)
             break;
     }
 
 
+}
+
+function exibirListaTarefas(tarefas = [], filtro = false) {
+    if (tarefas.length == 0 && filtro == false) {
+        tarefas = registro.receberTarefas()
+    }
+
+    let listaTarefas = document.getElementById('listaTarefas')
+    listaTarefas.innerHTML = ''
+
+    tarefas.forEach(function (t) {
+        let inserirLinha = listaTarefas.insertRow()
+        inserirLinha.insertCell(0).innerHTML = t.descricao
+        inserirLinha.insertCell(1).innerHTML = t.data
+        inserirLinha.insertCell(2).innerHTML = t.prioridade
+        inserirLinha.insertCell(3).innerHTML = t.status
+        let deletar = document.createElement("button")
+        deletar.className = 'btn btn-primary'
+        deletar.innerHTML = '<i class="fa-regular fa-trash-can"></i>'
+        deletar.id = `id_tarefa_${t.id}`
+        deletar.onclick = function() {
+            let id = this.id.replace('id_tarefa_', '')
+            registro.remover(id)
+            let linha = this.closest('tr')
+            if(linha) {
+                linha.remove()
+            }
+            exibirModal(2, 'sucesso')
+
+        }
+        inserirLinha.insertCell(4).append(deletar)
+    })
 }
 
 function selecionarTarefa(valor) {
@@ -113,7 +176,7 @@ function exibirModal(valor, tipo) {
         return $('#exibirModal').modal('show')
     }
     document.getElementById('titulo-modal').innerHTML = 'Sucesso!'
-    document.getElementById('conteudo-modal').innerHTML = 'Sua tarefa foi adicionada com sucesso!'
+    document.getElementById('conteudo-modal').innerHTML = valor == 1 ? 'Sua tarefa foi adicionada com sucesso!' : 'Sua tarefa foi excluída com sucesso!'
     document.getElementById('estilo-modal').classList.add('text-success')
     document.getElementById('botao-config').innerHTML = 'Voltar'
     document.getElementById('botao-config').classList.add('btn-success')
